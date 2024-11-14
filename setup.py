e77376f0b06d54654dbad9803291944ee921beb5
Jon Geater <jon.geater@rkvst.com>
2024-11-14 10:51:43 +0100

    Release bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="rfc9290",
-    version="0.1.0",
+    version="0.1.1",
     description="Simple encoder/decoder for RFC 9290 Concise Problem Details",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
